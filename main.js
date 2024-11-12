6422954b7ccb27eaf41adbbbdfbef4fb636e0c5f
alb-soul <xeamam.hsu@gmail.com>
2024-11-12 19:41:14 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+    icon: path.join(__dirname, 'gpt.png'),  // Menambahkan ikon aplikasi
+    autoHideMenuBar: true,  // Menyembunyikan menu bar
+  });
+
+  win.loadURL('https://chatgpt.com');
+
+  // Menangani ketika jendela ditutup
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+// Menangani kapan semua jendela ditutup
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
