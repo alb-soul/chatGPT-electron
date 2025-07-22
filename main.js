19fbc91e13902f76af79b9cc866c7ce49dc7c792
Alb-soul <xeamam.hsu@gmail.com>
2025-07-22 08:07:59 +0700

    Update main.js to use smaller window initiation size

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ let win;
 
 function createWindow() {
   win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 900,
+    height: 600,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
